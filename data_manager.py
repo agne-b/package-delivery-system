d5356b0bb4577f4952380fbf10ba6d6536ca5f71
agne-b <agne.banitauskaite@stud.vilniustech.lt>
2026-04-19 20:39:41 +0300

    Create data_manager.py

---
+++ data_manager.py
@@ -0,0 +1,12 @@
+import csv
+from package_factory import PackageFactory
+from address import Address
+
+class DataManager:
+  FILE_NAME = "packages.csv"
+
+  @staticmethod
+  def save_packages(packages):
+    with open(DataManager.FILE_NAME, mode="w", newline="") as file:
+      writer = csv.writer(file)
+      
