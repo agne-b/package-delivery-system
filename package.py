88a79647563af305af12b234977af68db0a1e43e
agne-b <agne.banitauskaite@stud.vilniustech.lt>
2026-04-19 13:15:37 +0300

    Create package.py

---
+++ package.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+
+class Package(ABC):
+    def __init__(self, package_id, weight, sender, receiver):
+        self._package_id = package_id
+        self._weight = weight
+        self._sender = sender
+        self._receiver = receiver
+        self._status = "Created"
+
+    def get_status(self):
+        return self._status
+    
+    def update_status(self, new_status):
+        self._status = new_status
+
+    @abstractmethod
+    def calculate_cost(self):
+        pass
