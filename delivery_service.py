9b3773a434d980c5e39b238ccdb28cdfbeaac32e
agne-b <agne.banitauskaite@stud.vilniustech.lt>
2026-04-19 13:20:01 +0300

    Create delivery_service.py

---
+++ delivery_service.py
@@ -0,0 +1,33 @@
+from standard_package import StandardPackage
+from express_package import ExpressPackage
+
+class DeliveryService:
+    def __init__(self):
+        self._packages = []
+        self._couriers = []
+
+    def add_courier(self, courier):
+        self._couriers.append(courier)
+
+    def create_standard_package(self, package_id, weight, sender, receiver):
+        package = StandardPackage(package_id, weight, sender, receiver)
+        self._packages.append(package)
+        return package
+
+    def create_express_package(self, package_id, weight, sender, receiver):
+        package = ExpressPackage(package_id, weight, sender, receiver)
+        self._packages.append(package)
+        return package
+
+    def assign_package_to_courier(self, package, courier):
+        courier.assign_package(package)
+
+    def calculate_total_shipping_cost(self):
+        total = 0
+        for p in self._packages:
+            total += p.calculate_cost()
+        return total
+
+    def list_all_packages(self):
+        for p in self._packages:
+            print(f"Package {p._package_id}, Status: {p.get_status()}, Type: {p.get_type()}")
