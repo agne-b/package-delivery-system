baa90c9e0901b2e5dbdee354e558b98115b33c6c
agne-b <agne.banitauskaite@stud.vilniustech.lt>
2026-04-22 21:38:02 +0300

    Update data_manager.py

--- data_manager.py
+++ data_manager.py
@@ -2,13 +2,61 @@ import csv
 from package_factory import PackageFactory
 from address import Address
 
+
 class DataManager:
-  FILE_NAME = "packages.csv"
+    FILE_NAME = "packages.csv"
 
-  @staticmethod
-  def save_packages(packages):
-    with open(DataManager.FILE_NAME, mode="w", newline="") as file:
-      writer = csv.writer(file)
+    @staticmethod
+    def save_packages(packages):
+        file = open(DataManager.FILE_NAME, "w", newline="")
+        writer = csv.writer(file)
 
-      
-      
+        writer.writerow(["id", "type", "weight", "sender", "receiver", "status"])
+
+        for p in packages:
+            writer.writerow([
+                p.package_id,
+                p.get_type(),
+                p._weight,
+                p._sender,
+                p._receiver,
+                p.get_status()
+            ])
+
+        file.close()
+
+    @staticmethod
+    def load_packages():
+        packages = []
+
+        try:
+            file = open(DataManager.FILE_NAME, "r")
+            reader = csv.reader(file)
+
+            next(reader)
+
+            for row in reader:
+                package_id = row[0]
+                package_type = row[1]
+                weight = float(row[2])
+                sender = row[3]
+                receiver = row[4]
+                status = row[5]
+
+                package = PackageFactory.create_package(
+                    package_type,
+                    package_id,
+                    weight,
+                    sender,
+                    receiver
+                )
+
+                package.update_status(status)
+                packages.append(package)
+
+            file.close()
+
+        except FileNotFoundError:
+            return []
+
+        return packages
