de9ae0dd8127a63a9d92cac8b17be16e6460b515
agne-b <agne.banitauskaite@stud.vilniustech.lt>
2026-04-22 15:45:31 +0300

    Update address.py

--- address.py
+++ address.py
@@ -1,5 +1,11 @@
 class Address:
     def __init__(self, street, city, postal_code):
+        if not street:
+            raise ValueError("Street cannot be empty")
+        if not city:
+            raise ValueError("City cannot be empty")
+        if not postal_code:
+            raise ValueError("Postal code cannot be empty")
         self.street = street
         self.city = city
         self.postal_code = postal_code
