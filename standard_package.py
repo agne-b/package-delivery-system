270458d01193d4e9d32fe4e05bb6a470a857c069
agne-b <agne.banitauskaite@stud.vilniustech.lt>
2026-04-22 20:53:38 +0300

    Update standard_package.py

--- standard_package.py
+++ standard_package.py
@@ -1,12 +1,14 @@
 from package import Package
 
 class StandardPackage(Package):
-    def __init__(self, package_id, weight, sender, receiver):
+    def __init__(self, package_id, weight, sender, receiver, base_rate=2.0):
         super().__init__(package_id, weight, sender, receiver)
-        self.base_rate = 2.0
+        if base_rate <= 0:
+            raise ValueError("Base rate must be positive")
+        self._base_rate = base_rate
 
     def calculate_cost(self):
-        return self.base_rate * self._weight
+        return round(self._base_rate * self._weight, 2)
 
     def get_type(self):
         return "Standard"
