8b8c25425a3f3fa998f8fc183f6f870f29f56d64
agne-b <agne.banitauskaite@stud.vilniustech.lt>
2026-04-25 14:47:38 +0300

    Create test.package.py

---
+++ test.package.py
@@ -0,0 +1,23 @@
+import unittest
+from standard_package import StandardPackage
+
+class TestPackage(unittest.TestCase):
+
+  def create_sender_and_receiver(self):
+    self.sender = Address("Street 1", "City 1", "12345")
+    self.receiver = Address("Street 2", "City 2", "56789")
+    
+  def test_valid_package_creation(self):
+    package = StandardPackage("12345", 2.5, self.sender, self.receiver)
+    self.assertEqual(package.package_id, "12345")
+    self.assertEqual(package.weight, 2.5)
+    self.assertEqual(package.get_status(), "Created")
+
+def test_invalid_package_id_letters(self):
+  with self.assertRaises(ValueError):
+    StandardPackage("12A45", 2.5, self.sender, self.receiver)
+
+def test_invalid_weight(self):
+  with self.assertRaises(ValueError):
+    StandardPackage("12345", -2.5, self.sender, self.receiver)
+    
