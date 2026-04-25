6fbac55a51856f0ff1f89af1458288dfa1112a37
agne-b <agne.banitauskaite@stud.vilniustech.lt>
2026-04-25 15:16:39 +0300

    Update test_package_factory.py

--- test_package_factory.py
+++ test_package_factory.py
@@ -3,18 +3,21 @@ from package_factory import PackageFactory
 from address import Address
 
 class TestPackageFactory(unittest.TestCase):
-  def setup(self):
+  def setUp(self):
     self.sender = Address("Street 1", "City", "12345")
     self.receiver = Address("Street 2", "City", "56789")
 
-def test_create_standard(self):
-  package = PackageFactory.create_package("Standard", "44444", 1.0, self.sender, self.receiver)
-  self.assertEqual(package.get_type(), "Standard")
+  def test_create_standard(self):
+    package = PackageFactory.create_package("Standard", "44444", 1.0, self.sender, self.receiver)
+    self.assertEqual(package.get_type(), "Standard")
 
-def test_create_express(self):
-  package = PackageFactory.create_package("Express", "55555", 1.0, self.sender, self.receiver)
-  self.assertEqual(package.get_type(), "Express")
+  def test_create_express(self):
+    package = PackageFactory.create_package("Express", "55555", 1.0, self.sender, self.receiver)
+    self.assertEqual(package.get_type(), "Express")
 
-def test_invalid_type(self):
-   with self.assertRaises(ValueError):
-     PackageFactory.create_package("Fast", "66666", 1.0, self.sender, self.receiver)
+  def test_invalid_type(self):
+     with self.assertRaises(ValueError):
+       PackageFactory.create_package("Fast", "66666", 1.0, self.sender, self.receiver)
+
+if __name__ == "__main__": 
+  unittest.main()
